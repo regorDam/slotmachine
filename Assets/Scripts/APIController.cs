db7a692f4ae59ef1a231ca8e35a068e64a322db7
regorDam <regorm88@gmail.com>
2021-04-08 15:26:57 +0200

    menu API clock

---
+++ Assets/Scripts/APIController.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class APIController : MonoBehaviour
+{
+    [SerializeField]
+    string MAIN_URL = "http://worldtimeapi.org/api/ip";
+
+    APITime apiTime;
+    Text clock;
+    private string ReadJsonFromAPI(string URL)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+
+        string jsonResponse = reader.ReadToEnd();
+        return jsonResponse;
+    }
+
+    private void Awake()
+    {
+        clock = GetComponent<Text>();
+        apiTime = new APITime();
+    }
+
+    private void Start()
+    {
+        SetClock();
+    }
+
+    private void Update()
+    {
+        SetClock();
+    }
+
+    private void SetClock()
+    {
+        JsonUtility.FromJsonOverwrite(ReadJsonFromAPI(MAIN_URL), apiTime);
+        System.DateTime dateTime = System.DateTime.Parse(apiTime.datetime);
+        
+        clock.text = dateTime.ToString("yyyy-MM-dd hh:mm:ss");
+    }
+}
---
+++ Assets/Scripts/APITime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Serializable]
+public class APITime
+{
+    public string abbreviation;
+    public string client_ip;
+    public string datetime;
+    //day_of_week":4,"day_of_year":247,"dst":true,"dst_from":"2020-03-29T01:00:00+00:00","dst_offset":3600,"dst_until":"2020-10-25T01:00:00+00:00","raw_offset":3600,"timezone":"Europe/Madrid","unixtime":1599142336,"utc_datetime":"2020-09-03T14:12:16.328503+00:00","utc_offset":"+02:00","week_number":36
+}
