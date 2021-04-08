afa993e751cad6df592e171903fe8030feccd59b
regorDam <regorm88@gmail.com>
2021-04-08 17:43:16 +0200

    slot machine with spin sound button click

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -42,4 +42,9 @@ public class GameController : MonoBehaviour
     {
         LevelManager.Instance.LoadMenu();
     }
+
+    public void OnClickSpinBtn(GameObject button)
+    {
+        button.GetComponent<AudioSource>().Play();        
+    }
 }
